80fb151ce5923aa4eb85bdaf3485e62a16d49d68
AStokho <79436041+AStokho@users.noreply.github.com>
2021-02-22 12:18:24 -0800

    module 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -26,9 +26,27 @@ namespace GradeBook.GradeBooks
             else if (averageGrade >= 20)
                 return 'D';
             else
-                return 'F';
+                return 'F';            
+        }
 
-            
+        public override void CalculateStatistics()
+        {
+            if(Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");                
+            }
+            else
+                base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if(Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            else
+                base.CalculateStudentStatistics(name);
         }
     }
 }
